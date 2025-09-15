923e136df4e3592aba1d51f8e229d8d3030dfb62
ereyn <gogra.ereyn@gmail.com>
2025-09-15 21:41:11 +0100

    filepv

--- dot_bash_core/utils.sh
+++ dot_bash_core/utils.sh
@@ -9,6 +9,10 @@ fpv() {
     pv -bartF '%t %a %r %b' > /dev/null
 }
 
+filepv() {
+    pv -f -i 1 -F '%t %a %r %b' >/dev/null 2> >(stdbuf -o0 tr '\r' '\n' >> ${OUTFILE:-./pv_out.log})
+}
+
 pss() {
     ps --sort=start_time "$@"
 }
