a84944b71d353cd5b0ca77a945b791357919e55d
Gogra Ereyn <gogra.ereyn@gmail.com>
2025-09-21 17:29:32 +0100

    vim markdown

---
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        'plasticboy/vim-markdown',
+        ft = { "markdown" },
+        dependencies = {
+            'godlygeek/tabular',
+        },
+        config = function()
+            -- never ever fold!
+            vim.g.vim_markdown_folding_disabled = 1
+            -- support front-matter in .md files
+            vim.g.vim_markdown_frontmatter = 1
+            -- 'o' on a list item should insert at same level
+            vim.g.vim_markdown_new_list_item_indent = 0
+            -- don't add bullets when wrapping:
+            -- https://github.com/preservim/vim-markdown/issues/232
+            vim.g.vim_markdown_auto_insert_bullets = 0
+        end
+    },
+}
