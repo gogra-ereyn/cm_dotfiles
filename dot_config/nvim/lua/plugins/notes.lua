6c153646a06a57d4c28f400b4e8c253a5954c55f
Gogra Ereyn <gogra.ereyn@gmail.com>
2026-01-07 23:58:26 +0000

    fixing obsidian config

--- dot_config/nvim/lua/plugins/notes.lua
+++ dot_config/nvim/lua/plugins/notes.lua
@@ -1,30 +1,33 @@
 return {
-  "obsidian-nvim/obsidian.nvim",
-  version = "*",
-  ft = "markdown",
-  ---@module 'obsidian'
-  ---@type obsidian.config
-  opts = {
-    legacy_commands = false,
-    workspaces = {
-      {
-        name = "personal",
-        path = "/apps/obsidian/myvault",
-      }
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
+    ft = "markdown",
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+        legacy_commands = false,
+        workspaces = {
+            {
+                name = "personal",
+                path = "/apps/obsidian/myvault",
+            }
+        },
+        daily_notes = {
+            folder = "todos"
+        },
     },
-  },
-  config=function()
-    vim.keymap.set("n", "<leader>od", "<cmd>ObsidianToday<cr>")
-    vim.keymap.set("n", "<leader>oy", "<cmd>ObsidianYesterday<cr>")
-    vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTomorrow<cr>")
-    vim.keymap.set("n", "<leader>oD", "<cmd>ObsidianDailies<cr>")
+    config = function(_, opts)
+        require("obsidian").setup(opts)
+        vim.keymap.set("n", "<leader>od", "<cmd>Obsidian today<cr>")
+        vim.keymap.set("n", "<leader>oy", "<cmd>Obsidian yesterday<cr>")
+        vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian tomorrow<cr>")
+        vim.keymap.set("n", "<leader>oD", "<cmd>Obsidian dailies<cr>")
 
-    vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianQuickSwitch<cr>")
-    vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<cr>")
-    vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<cr>")
-    vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<cr>")
+        vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian quick_switch<cr>")
+        vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<cr>")
+        vim.keymap.set("n", "<leader>ob", "<cmd>Obsidian backlinks<cr>")
+        vim.keymap.set("n", "<leader>on", "<cmd>Obsidian create<cr>")
 
-    vim.keymap.set("v", "<leader>ol", "<cmd>ObsidianLink<cr>")
-    vim.keymap.set("v", "<leader>oL", "<cmd>ObsidianLinkNew<cr>")
-end
+        vim.keymap.set("v", "<leader>ol", "<cmd>Obsidian link<cr>")
+    end,
 }
