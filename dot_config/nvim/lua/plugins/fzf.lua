3628bfb79c8900a8d5613e16d030ff828b4014d1
Gogra Ereyn <gogra.ereyn@gmail.com>
2026-05-08 01:39:37 +0100

    revert fzf

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -27,8 +27,6 @@ return {
                 },
             }
 
-            require 'fzf-lua'.register_ui_select()
-
             -- jon-gj 's proximity sort.
             -- can be installed via `cargo install proximity-sort`
             vim.keymap.set('', '<C-p>', function()
